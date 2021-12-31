c78a4997621647aeae4c79ab924d39dcf80be228
Li Ling <liling@kth.se>
2021-12-31 17:34:32 -0300

    Modify utils function for reading icefront data.

--- utils.py
+++ utils.py
@@ -1,16 +1,23 @@
 import sys
 
-def convert_icefront_to_huginos_format(filename):
+
+def convert_icefront_to_huginos_format(filenames):
     output = []
-    with open(filename, 'r', encoding='utf-8') as f:
-        for i, line in enumerate(f):
-            if i == 0:
-                output.append(''.join([line.strip(), '\t10']))
-            else:
-                output.append(''.join([line.strip(), '\t15']))
-    output_file = f'{filename.split(".")[0]}-huginos.txt'
+    for filename in filenames:
+        with open(filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                if len(output) == 0:
+                    output.append(''.join([line.strip(), '\t10']))
+                else:
+                    output.append(''.join([line.strip(), '\t15']))
+
+    date = filenames[0].split('.')[0].split('_')[-1]
+    output_file = f'icefront-{date}.txt'
     with open(output_file, 'w', encoding='utf-8') as f:
         f.writelines('\n'.join(output))
 
+
 if __name__ == '__main__':
-    convert_icefront_to_huginos_format(sys.argv[1])
+    files = [x for x in sys.argv[1:]]
+    print(files)
+    convert_icefront_to_huginos_format(files)
