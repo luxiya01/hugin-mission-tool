8ef42b09bc87dd764bd053fc0c1eeba925b152bf
Li Ling <liling@kth.se>
2022-01-13 20:37:30 -0300

    Add conversion for polwarview jp2 data

--- map_conversion.py
+++ map_conversion.py
@@ -5,6 +5,13 @@ from consts import *
 from tqdm import tqdm
 
 
+def gdalwarp_polarview_jp2(source_file: str, output_file: str):
+    subprocess.run([
+        'gdalwarp', '-s_srs', 'EPSG:3031', '-t_srs', 'EPSG:4326', source_file,
+        output_file
+    ])
+
+
 def gdalwarp_AMSR2_seaice_concentration(source_file: str, output_file: str):
     output = subprocess.run([
         'gdalwarp', '-s_srs', 'EPSG:3412', '-t_srs', 'EPSG:4326', '-te',
@@ -15,9 +22,17 @@ def gdalwarp_AMSR2_seaice_concentration(source_file: str, output_file: str):
     print(output)
 
 
+def gdalwarp_MODIS_seaice_image(source_file: str, output_file: str):
+    subprocess.run([
+        'gdalwarp', '-t_srs', '+proj=longlat +datum=WGS84 +no_defs',
+        source_file, output_file
+    ])
+
+
 def amsr2_to_hugin_folder():
-    amsr2_tif_source = set(x for x in os.listdir(SEA_ICE_CONCENTRATION)
-                           if x.split('.')[-1] == 'tif')
+    amsr2_tif_source = set(
+        x for x in os.listdir(SEA_ICE_CONCENTRATION)
+        if x.split('.')[-1] == 'tif' and 'Antarctic_AMSR2' in x)
     amsr2_tif_target = set(os.listdir(HUGIN_MAPS_AMSR2_SEA_ICE_CONCENTRATION))
 
     for source in tqdm(amsr2_tif_source):
@@ -28,21 +43,17 @@ def amsr2_to_hugin_folder():
         gdalwarp_AMSR2_seaice_concentration(source_file, output_file)
 
 
-def gdalwarp_MODIS_seaice_image(source_file: str, output_file: str):
-    subprocess.run([
-        'gdalwarp', '-t_srs', '+proj=longlat +datum=WGS84 +no_defs',
-        source_file, output_file
-    ])
-
-
 def seaice_modis_data():
     modis_tif_source = set(x for x in os.listdir(SEA_ICE_IMAGERY)
                            if x.split('.')[-1] == 'tiff')
-    modis_tif_target = set(os.listdir(HUGIN_MAPS_MODIS_SEA_ICE_IMAGERY))
+    modis_tif_target = set(
+        x.replace('.tiff', '.tif')
+        for x in os.listdir(HUGIN_MAPS_MODIS_SEA_ICE_IMAGERY))
 
     for source in tqdm(modis_tif_source):
         if source in modis_tif_target:
             continue
         source_file = os.path.join(SEA_ICE_IMAGERY, source)
         output_file = os.path.join('./data/MODIS', source)
+        output_file = output_file.replace('.tiff', '.tif')
         gdalwarp_MODIS_seaice_image(source_file, output_file)
