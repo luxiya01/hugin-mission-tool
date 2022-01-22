98c7f41925af666a1869065e9c372a82361e4bb1
Li Ling <liling@kth.se>
2022-01-22 13:30:05 -0300

    Refactoring

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ server = app.server
 
 #TODO: remove global variable for holding mission...
 CURRENT_MISSION = None
+TIME_FMT = '%Y-%m-%d, %H:%M'
 
 #TODO: move id assignments to a components_id.py const file
 app.layout = html.Div([
@@ -44,29 +45,53 @@ app.layout = html.Div([
 ])
 
 
-def mission_file_to_dataframe(contents, filename, start_time, time_fmt):
-    """Given a content stream from dcc.Upload, filename, start_time and time_fmt,
+def parse_mission_file_content(contents, filename):
+    """Given a content stream from dcc.Upload, filename,
     return a pandas dataframe with waypoints"""
     _, content_str = contents.split(',')
     try:
-        m = MissionParser.parse_upload(filename=filename,
-                                       content_str=content_str)
+        return MissionParser.parse_upload(filename=filename,
+                                          content_str=content_str)
     except Exception as e:
         return None
 
+
+def mission_to_dataframe(start_time):
     global CURRENT_MISSION
-    CURRENT_MISSION = m
-    df = pd.DataFrame(m.mission)
-    df['lat'] = [x.latitude_in_dd for x in m.mission]
-    df['lon'] = [x.longitude_in_dd for x in m.mission]
-    df['timestamp'] = m.compute_mission_timestamps(start_time)
-    df['reaching_time'] = [t.strftime(time_fmt) for t in df['timestamp']]
+    df = pd.DataFrame(CURRENT_MISSION.mission)
+    df['lat'] = [x.latitude_in_dd for x in CURRENT_MISSION.mission]
+    df['lon'] = [x.longitude_in_dd for x in CURRENT_MISSION.mission]
+    df['timestamp'] = CURRENT_MISSION.compute_mission_timestamps(start_time)
+    df['reaching_time'] = [t.strftime(TIME_FMT) for t in df['timestamp']]
     df['reached'] = False
     df.loc[0, 'reached'] = True
     df = df[[
         'No', 'lat', 'lon', 'timestamp', 'reaching_time', 'reached', 'Comment'
     ]]
     return df
+
+
+def get_start_time_from_date_time(date, time):
+    time_str = f'{date}, {time}'
+    start_time = datetime.datetime.strptime(time_str, TIME_FMT)
+    return start_time
+
+
+def compute_geojson_based_on_df(df):
+    dicts = df.to_dict('rows')
+    for item in dicts:
+        tooltip = f'Waypoint No.{item["No"]}<br/>Position: ({item["lat"]:.2f}, {item["lon"]:.2f})<br/>Reaching time: {item["reaching_time"]}'
+        if item['Comment'] != '':
+            tooltip = f'{tooltip}<br/>Comment: {item["Comment"]}'
+        item['tooltip'] = tooltip
+    geojson_data = dlx.dicts_to_geojson(dicts)
+    return geojson_data
+
+
+def compute_polyline_based_on_df(df):
+    polyline_pos = [[lat, lon] for (lat, lon) in zip(df.lat, df.lon)
+                    if not np.isnan(lat) and not np.isnan(lon)]
+    return polyline_pos
 
 
 @app.callback(Output('upload-mission-file-output', 'children'),
@@ -80,34 +105,25 @@ def mission_file_to_dataframe(contents, filename, start_time, time_fmt):
               State('mission-start-time', 'value'), State('map', 'center'))
 def update_map_plot(n_clicks, contents, filename, date, time,
                     current_map_center):
+    global CURRENT_MISSION
     upload_mission_file_output = html.H5(f'Selected mission: {filename}')
-    time_str = f'{date}, {time}'
-    time_fmt = '%Y-%m-%d, %H:%M'
-    time_div = html.H5(f'Selected mission start time: {time_str}')
+
+    start_time = get_start_time_from_date_time(date, time)
+    time_div = html.H5(f'Selected mission start time: {start_time}')
     geojson_data = None
     polyline_pos = [[0, 0], [0, 0]]
     map_center = current_map_center
 
-    start_time = datetime.datetime.strptime(time_str, time_fmt)
-
     if contents is not None:
-        df = mission_file_to_dataframe(contents, filename, start_time,
-                                       time_fmt)
         #TODO: handle when mission parsing fails
+        m = parse_mission_file_content(contents, filename)
+        CURRENT_MISSION = m
+
+        df = mission_to_dataframe(start_time)
+        geojson_data = compute_geojson_based_on_df(df)
+        polyline_pos = compute_polyline_based_on_df(df)
         map_center = (df.lat.mean(), df.lon.mean())
 
-        # Compute Geojson based on waypoints from df
-        dicts = df.to_dict('rows')
-        for item in dicts:
-            tooltip = f'Waypoint No.{item["No"]}<br/>Position: ({item["lat"]:.2f}, {item["lon"]:.2f})<br/>Reaching time: {item["reaching_time"]}'
-            if item['Comment'] != '':
-                tooltip = f'{tooltip}<br/>Comment: {item["Comment"]}'
-            item['tooltip'] = tooltip
-        geojson_data = dlx.dicts_to_geojson(dicts)
-
-        # Compute polyline based on waypoints from df
-        polyline_pos = [[lat, lon] for (lat, lon) in zip(df.lat, df.lon)
-                        if not np.isnan(lat) and not np.isnan(lon)]
     return upload_mission_file_output, time_div, geojson_data, polyline_pos, map_center
 
 
@@ -118,13 +134,6 @@ def update_map_plot(n_clicks, contents, filename, date, time,
 def geojson_callback(n_clicks, click_feature):
     print(f'clicked feature: {click_feature}')
     lon, lat = click_feature['geometry']['coordinates']
-    global CURRENT_MISSION
-    CURRENT_MISSION.mission.append(
-        WayPoint(Latitude=WayPoint.degree_decimals_to_degree_minutes(
-            lat, is_lat=True),
-                 Longitude=WayPoint.degree_decimals_to_degree_minutes(
-                     lon, is_lat=False)))
-    print(f'current mission length: {CURRENT_MISSION.length}')
     return [
         dl.CircleMarker(center=[lat, lon],
                         children=dl.Tooltip("{}".format(click_feature)),
@@ -132,9 +141,12 @@ def geojson_callback(n_clicks, click_feature):
     ]
 
 
-@app.callback(Output('map-callback-layer', 'children'),
-              [Input('map', 'click_lat_lng')])
-def map_click(click_lat_lng):
+@app.callback(Output('map-callback-layer', 'children'), [
+    Input('map', 'click_lat_lng'),
+    State('mission-start-date', 'date'),
+    State('mission-start-time', 'value')
+])
+def map_click(click_lat_lng, date, time):
     return [
         dl.Marker(position=click_lat_lng,
                   children=dl.Tooltip(
