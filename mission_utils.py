75e625e0af39801662f7dcd0c2f9580078ce3b6a
Li Ling <liling@kth.se>
2022-02-02 21:58:54 -0300

    Add funcionalities to modify mission plan (.mp) flags based on comments

--- mission_utils.py
+++ mission_utils.py
@@ -1,20 +1,132 @@
 import datetime
 import pandas as pd
+import os
+from dataclasses import dataclass
 from mission_parser import MissionParser
 
 
-def convert_mission_file_to_latlon_csv(filename, start_time=None):
-    mission = MissionParser.parse_file(filename)
+@dataclass
+class Flag:
+    """
+    A class used to represent a Flag in Hugin missions.
+
+    Parameters
+    ----------
+    name: str
+        The name of the flag
+    command: str
+        The command/flag used in HuginOS
+    explanation: str
+        The meaning of the flag in plain text
+    """
+    name: str
+    flag: str
+    explanation: str
+
+
+START_WATER_SAMPLING_COMMENT = 'WS'
+START_WATER_SAMPLING_FLAG = Flag(
+    name='start_water_sampling',
+    flag='cmd=9C013C',
+    explanation='Start collecting water sample for one bottle')
+STOP_WATER_SAMPLING_FLAG = Flag(
+    name='stop_water_sampling',
+    flag='cmd=9C012C',
+    explanation='Stop collecting water sample for one bottle')
+COMMENT_TO_FLAG = {START_WATER_SAMPLING_COMMENT: START_WATER_SAMPLING_FLAG}
+
+
+def add_start_water_sampling_flag_to_mission(filepath: str):
+    add_flag_according_to_comment(
+        filepath=filepath,
+        comment=START_WATER_SAMPLING_COMMENT,
+        flag=COMMENT_TO_FLAG[START_WATER_SAMPLING_COMMENT].flag)
+
+
+def add_flag_according_to_comment(filepath: str, comment: str, flag: str):
+    """Given a mission file, look for lines with the desired comment and add the
+    flag specified to the line. The modified mission is written back to the same
+    folder with a new filename specifying the comment used to add the flag.
+
+    Parameters
+    ----------
+    filepath: str
+        Path to the .mp file.
+    comment: str
+        The string comment to look for.
+    flag: str
+        The string representation of the flag to be added to the mission line under
+        the line with the specified comment.
+    """
+    comment_str = f'# {comment}'
+    add_flag = False
+    modified_mission = []
+    with open(filepath, 'r') as f:
+        lines = [l.strip('\n') for l in f.readlines()]
+        for line in lines:
+            modified_line = line
+
+            if add_flag:
+                # separator is used to align the values in the Flag column
+                separator = ' ' if line[-1] == ')' else '  '
+                modified_line = separator.join([line, flag])
+                add_flag = False
+            modified_mission.append(modified_line)
+
+            if comment_str in line:
+                add_flag = True
+
+    folder = os.path.dirname(filepath)
+    filename = os.path.basename(os.path.normpath(filepath))
+    output_filename = f'{filename.split(".")[0]}_add_{comment}_flag.mp'
+    output_filepath = os.path.join(folder, output_filename)
+    with open(output_filepath, 'w') as f:
+        f.writelines('\n'.join(modified_mission))
+
+
+def convert_mission_file_to_latlon_csv(
+        filepath: str,
+        start_time=None,
+        remove_rendezvous=False) -> pd.DataFrame:
+    """Convert a mission file into a csv file with lat, lon and (if start_time is not
+    None) timestamp for the time completing each line. WayPoints without explicit lat,
+    lon values will be removed.
+
+    Parameters
+    ----------
+    filepath: str
+        Path to the .mp mission file.
+    start_time: str, default=None
+        Datetime represented in %Y-%m-%d %H:%M, used to compute the time reaching
+        each waypoint.
+    remove_rendezvous: bool, default=False
+        If set to True, all WayPoints with "rendezvous" as their Flags will be removed
+        prior to conversion to .csv file.
+
+    Returns
+    -------
+    df: pd.DataFrame
+        A pandas dataframe containing lat, lon and (if start_time is not
+        None) timestamp of the time this waypoint is completed. The dataframe is
+        also stored as a .csv using the same filepath as the input filepath parameter.
+    """
+    mission = MissionParser.parse_file(filepath).mission
+    if remove_rendezvous:
+        mission = [m for m in mission if m.Flags != 'rendezvous']
     df = pd.DataFrame()
-    df['lat'] = [x.latitude_in_dd for x in mission.mission]
-    df['lon'] = [x.longitude_in_dd for x in mission.mission]
+    df['lat'] = [x.latitude_in_dd for x in mission]
+    df['lon'] = [x.longitude_in_dd for x in mission]
+
+    # Remove WayPoints without (lat, lon)
+    df = df[~df.lat.isna() & ~df.lon.isna()]
 
     if start_time is not None:
-        TIME_FMT = '%Y-%m-%d, %H:%M'
+        TIME_FMT = '%Y-%m-%d %H:%M'
         try:
             start_time = datetime.datetime.strptime(start_time, TIME_FMT)
         except Exception as e:
             print(e)
             return
         df['timestamp'] = mission.compute_mission_timestamps(start_time)
-    return df.to_csv(f'{filename.split(".")[0]}.csv', index=False)
+    df.to_csv(f'{filepath.split(".")[0]}.csv', index=False)
+    return df
