e64b42635637d8292616f404b86d449a623bc7f4
Li Ling <liling@kth.se>
2022-02-07 06:40:16 -0300

    Refactor functions for parsing CP-Events.txt and VehicleCTD.txt

--- mission_utils.py
+++ mission_utils.py
@@ -8,6 +8,42 @@ from flags import START_WATER_SAMPLING, STOP_WATER_SAMPLING
 from utils import utc_time_str_to_unixtime
 
 
+def get_lat_lon_depth_from_vehicle_ctd(
+        vehicle_ctd: pd.DataFrame,
+        unixtime: float,
+        max_time_diff: float = 60) -> (float, float, float):
+    """Given a vehicle_ctd dataframe, a target unix time and an allowed max_time_diff,
+    return the (lat, lon, depth) tuple from the vehicle_ctd at the first timestamp greater
+    than the target unix time.
+
+    Parameters
+    ----------
+    vehicle_ctd: pd.DataFrame
+        The Pandas DataFrame constructed from the VehicleCTD.txt
+        This is the value returned by the function vehicle_ctd_to_df
+    unixtime: float
+        The target unix time
+    max_time_diff: float
+        The maximum allowed time difference between the returned vehicle_ctd value and the
+        target unixtime (in seconds)
+
+    Returns
+    -------
+    lat: float
+    lon: float
+    depth: float
+    """
+    results = vehicle_ctd[vehicle_ctd.unixtime > unixtime]
+    if len(results) <= 0:
+        return
+    idx = results.index[0]
+    ctd_time = vehicle_ctd.loc[idx, 'unixtime']
+    if abs(unixtime - ctd_time) > max_time_diff:
+        return
+    return (vehicle_ctd.loc[idx, 'lat'], vehicle_ctd.loc[idx, 'lon'],
+            vehicle_ctd.loc[idx, 'depth'])
+
+
 def vehicle_ctd_to_df(vehicle_ctd_path: str) -> pd.DataFrame:
     """Given the path to the VehicleCTD.txt file from a Hugin mission, return
     a pandas DataFrame from the file.
@@ -15,7 +51,8 @@ def vehicle_ctd_to_df(vehicle_ctd_path: str) -> pd.DataFrame:
     Parameters
     ----------
     vehicle_ctd_path: str
-        Path to the vehicle ctd file
+        Path to the vehicle ctd file.
+        This file is found at mission_folder/env/ctd/VehicleCTD.txt
 
     Returns
     -------
@@ -31,6 +68,75 @@ def vehicle_ctd_to_df(vehicle_ctd_path: str) -> pd.DataFrame:
                               sep='\t',
                               names=vehicle_ctd_cols)
     return vehicle_ctd
+
+
+def parse_cp_events(cp_event_path: str) -> pd.DataFrame:
+    """Given path the CP-Events.txt file in a Hugin mission folder,
+    return a DataFrame with information on when each line in the mission
+    plan was completed. When parsing the CP-Events.txt, the time that mission
+    line i is read is used as the time that mission line (i-1) is completed.
+
+    Parameters
+    ----------
+    cp_event_path: str
+        Path to the CP-Events.txt
+        This file is found at mission_folder/cp/EventLog/CP-Events.txt
+
+    df: pd.DataFrame
+        A pandas dataframe with the following columns:
+            [name, waypoint_nr, time_str (UTC), unixtime]
+    """
+    mission_line_timestamps = []
+    cp_time_fmt = '%Y.%m.%d %H:%M:%S'
+    cp_line_pattern = re.compile(
+        r'(\d{4}.\d{2}.\d{2} \d{2}:\d{2}:\d{2}).\d{6}\s+([\w\s\(\)]+):\s+([\w\s]+):\s+(.+)'
+    )
+    mission_plan_read_pattern = re.compile(r'Mission plan read (\d*)')
+    nose_released_pattern = re.compile('Nose released')
+    with open(cp_event_path, 'r', encoding='utf-8') as cp_event_file:
+        cp_events = cp_event_file.readlines()
+    for i, line in enumerate(cp_events):
+        line = line.strip()
+        parsed_line = re.match(cp_line_pattern, line)
+
+        # The first few lines of the CP-Events.txt has a different format and can be ignored
+        if not parsed_line:
+            continue
+
+        time_str = parsed_line.group(1)
+        unixtime = utc_time_str_to_unixtime(time_str=time_str,
+                                            time_fmt=cp_time_fmt)
+        event_details = parsed_line.group(4)
+        mission_plan_read_match = re.search(mission_plan_read_pattern,
+                                            event_details)
+
+        if mission_plan_read_match:
+            mission_line_read = int(mission_plan_read_match.group(1))
+            if mission_line_read > 2:
+                mission_line_reached = mission_line_read - 1
+                mission_line_timestamps.append([
+                    f'Waypoint number {mission_line_reached}',
+                    mission_line_reached, time_str, unixtime
+                ])
+            continue
+
+        nose_released_match = re.search(nose_released_pattern, event_details)
+        if nose_released_match:
+            mission_line_timestamps.append(
+                ['recovery_start', -1, time_str, unixtime])
+
+        if i == (len(cp_events) - 1):
+            mission_line_timestamps.append(
+                ['recovery_ends', -1, time_str, unixtime])
+    df = pd.DataFrame(
+        mission_line_timestamps,
+        columns=['name', 'waypoint_nr', 'time_str (UTC)', 'unixtime'])
+    return df
+
+
+def parse_water_sample_locations_and_time(mission_folder: str,
+                                          save: bool = True) -> pd.DataFrame:
+    pass
 
 
 def parse_mission_data_for_waypoint_locations_and_time(mission_folder: str,
@@ -69,87 +175,48 @@ def parse_mission_data_for_waypoint_locations_and_time(mission_folder: str,
 
     # Parse CP-Events
     cp_event_path = os.path.join(mission_folder, 'cp/EventLog/CP-Events.txt')
-    cp_time_fmt = '%Y.%m.%d %H:%M:%S'
-    cp_line_pattern = re.compile(
-        r'(\d{4}.\d{2}.\d{2} \d{2}:\d{2}:\d{2}).\d{6}\s+([\w\s\(\)]+):\s+([\w\s]+):\s+(.+)'
-    )
-    mission_plan_read_pattern = re.compile(r'Mission plan read (\d*)')
-    nose_released_pattern = re.compile('Nose released')
-    with open(cp_event_path, 'r', encoding='utf-8') as cp_event_file:
-        cp_events = cp_event_file.readlines()
-    for i, line in enumerate(cp_events):
-        line = line.strip()
-        parsed_line = re.match(cp_line_pattern, line)
+    cp_event_df = parse_cp_events(cp_event_path)
 
-        # The first few lines of the CP-Events.txt has a different format and can be ignored
-        if not parsed_line:
-            continue
-
-        time_str = parsed_line.group(1)
-        unixtime = utc_time_str_to_unixtime(time_str=time_str,
-                                            time_fmt=cp_time_fmt)
-        event_details = parsed_line.group(4)
-        mission_plan_read_match = re.search(mission_plan_read_pattern,
-                                            event_details)
-        # Use the time that mission line i is read as the time reaching mission line (i-1)
-        if mission_plan_read_match:
-            mission_line_read = int(mission_plan_read_match.group(1))
-            if mission_line_read > 2:
-                mission_line_reached = mission_line_read - 1
-
-                # Get the first CTD line with time > the target unixtime
-                vehicle_ctd_idx = vehicle_ctd[
-                    vehicle_ctd.unixtime > unixtime].index[0]
-                mission_data.append([
-                    mission_line_reached,
-                    mission_plan.mission[mission_line_reached -
-                                         1].latitude_in_dd,
-                    mission_plan.mission[mission_line_reached -
-                                         1].longitude_in_dd, time_str,
-                    unixtime, vehicle_ctd.loc[vehicle_ctd_idx, 'lat'],
-                    vehicle_ctd.loc[vehicle_ctd_idx,
-                                    'lon'], vehicle_ctd.loc[vehicle_ctd_idx,
-                                                            'depth'],
-                    mission_plan.mission[mission_line_reached - 1].Latitude,
-                    mission_plan.mission[mission_line_reached - 1].Longitude,
-                    WayPoint.degree_decimals_to_degree_minutes(
-                        vehicle_ctd.loc[vehicle_ctd_idx, 'lat'], is_lat=True),
-                    WayPoint.degree_decimals_to_degree_minutes(
-                        vehicle_ctd.loc[vehicle_ctd_idx, 'lon'], is_lat=False)
-                ])
-            continue
-
-        nose_released_match = re.search(nose_released_pattern, event_details)
-        if nose_released_match:
-            # Get the first CTD line with time > the target unixtime
-            vehicle_ctd_idx = vehicle_ctd[
-                vehicle_ctd.unixtime > unixtime].index[0]
+    for row_idx, (name, waypoint_nr, time_str,
+                  unixtime) in cp_event_df.iterrows():
+        ctd_vals = get_lat_lon_depth_from_vehicle_ctd(vehicle_ctd, unixtime)
+        if not ctd_vals:
             mission_data.append([
-                'recovery_starts', '', '', time_str, unixtime,
-                vehicle_ctd.loc[vehicle_ctd_idx, 'lat'],
-                vehicle_ctd.loc[vehicle_ctd_idx,
-                                'lon'], vehicle_ctd.loc[vehicle_ctd_idx,
-                                                        'depth'], '', '',
-                WayPoint.degree_decimals_to_degree_minutes(
-                    vehicle_ctd.loc[vehicle_ctd_idx, 'lat'], is_lat=True),
-                WayPoint.degree_decimals_to_degree_minutes(
-                    vehicle_ctd.loc[vehicle_ctd_idx, 'lon'], is_lat=False)
-            ])
-        # Generally no CTD data here, so only record time string
-        if i == (len(cp_events) - 1):
-            mission_data.append([
-                'recovery_ends', '', '', time_str, unixtime, '', '', '', '',
+                name, waypoint_nr, time_str, unixtime, '', '', '', '', '', '',
                 '', '', ''
+            ])
+            continue
+        ctd_lat, ctd_lon, ctd_depth = ctd_vals
+
+        if waypoint_nr:
+            reached_waypoint = mission_plan.mission[waypoint_nr - 1]
+            mission_data.append([
+                name, waypoint_nr, time_str, unixtime,
+                reached_waypoint.latitude_in_dd,
+                reached_waypoint.longitude_in_dd, reached_waypoint.Latitude,
+                reached_waypoint.Longitude, ctd_lat, ctd_lon, ctd_depth,
+                WayPoint.degree_decimals_to_degree_minutes(ctd_lat,
+                                                           is_lat=True),
+                WayPoint.degree_decimals_to_degree_minutes(ctd_lon,
+                                                           is_lat=False)
+            ])
+        else:
+            mission_data.append([
+                name, waypoint_nr, time_str, unixtime, '', '', '', '', ctd_lat,
+                ctd_lon, ctd_depth,
+                WayPoint.degree_decimals_to_degree_minutes(ctd_lat,
+                                                           is_lat=True),
+                WayPoint.degree_decimals_to_degree_minutes(ctd_lon,
+                                                           is_lat=False)
             ])
 
     df = pd.DataFrame(mission_data,
                       columns=[
-                          'waypoint_nr', 'lat_from_mp', 'lon_from_mp',
-                          'timestamp (UTC)', 'unixtime',
-                          'lat_from_vehicle_ctd', 'lon_from_vehicle_ctd',
-                          'depth_from_vehicle_ctd', 'lat_from_mp_dm',
-                          'lon_from_mp_dm', 'lat_from_vehicle_ctd_dm',
-                          'lon_from_vehicle_ctd_dm'
+                          'name', 'waypoint_nr', 'timestamp (UTC)', 'unixtime',
+                          'lat_from_mp', 'lon_from_mp', 'lat_from_mp_dm',
+                          'lon_from_mp_dm', 'lat_from_vehicle_ctd',
+                          'lon_from_vehicle_ctd', 'depth_from_vehicle_ctd',
+                          'lat_from_vehicle_ctd_dm', 'lon_from_vehicle_ctd_dm'
                       ])
     if save:
         mission_name = os.path.basename(os.path.normpath(mission_folder))
