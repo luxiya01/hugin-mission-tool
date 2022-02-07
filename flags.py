89b9915ef2706588f6964f45835f903b95aa3549
Li Ling <liling@kth.se>
2022-02-07 06:40:33 -0300

    Add Flags dataclass

---
+++ flags.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+import re
+
+
+@dataclass
+class Flag:
+    """
+    A class used to represent a Flag in Hugin missions.
+
+    Parameters
+    ----------
+    name: str
+        The name of the flag
+    flag: str
+        The command/flag used in HuginOS
+    comment: str
+        The regex comment that this flag corresponds to
+    """
+    name: str
+    flag: str
+    comment: str
+
+
+START_WATER_SAMPLING = Flag(name='start_water_sampling',
+                            flag='cmd=9C013C',
+                            comment=r'WS \d+')
+STOP_WATER_SAMPLING = Flag(name='stop_water_sampling',
+                           flag='cmd=9C012C',
+                           comment='')
